08492a5f832fa4b24fb8b5566e0bf65d483bc4c7
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-04 23:11:03 +0530

    project intialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import { generateDate } from "./components/Calendar.js";
+
+export default function App() {
+  console.log(generateDate());
+  const days = ["S", "M", "T", "W", "T", "F", "S"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-96 h-96'>
+      <div className="w-full grid grid-cols-7">
+        {days.map((day, index) => {
+          return <h1 key={index}>{day}</h1>;
+        })}
+      </div>
+      <div className="w-full grid grid-cols-7">
+        {generateDate().map(({ date, currentMonth, today }, index) => {
+          return (
+            <div>
+              <h1 key={index}>{date.date()}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
