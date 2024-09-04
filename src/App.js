5b91862df12e7ceb22acfbc6ea8083d4a578b00d
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-05 00:47:46 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   console.log(generateDate());
   const days = ["S", "M", "T", "W", "T", "F", "S"];
   return (
-    <div className='w-96 h-96'>
+    <div className='w-56 h-96'>
       <div className="w-full grid grid-cols-7">
         {days.map((day, index) => {
           return <h1 key={index}>{day}</h1>;
